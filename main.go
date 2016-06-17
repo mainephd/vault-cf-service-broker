e438d51c217987d494422d878b90a2bd0eae1055
Davis, Jermaine <jermaine_a_davis@homedepot.com>
2016-06-17 09:13:45 -0400

    Adding return statements so the skeleton compiles properly

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ type vaultServiceBroker struct {}
 
 func (*vaultServiceBroker) Services() []brokerapi.Service {
 	// Return a []brokerapi.Service here, describing your service(s) and plan(s)
+	return []brokerapi.Service{}
 }
 
 func (*vaultServiceBroker) Provision(
@@ -20,31 +21,37 @@ asyncAllowed bool,
 ) (brokerapi.ProvisionedServiceSpec, error) {
 	// Provision a new instance here. If async is allowed, the broker can still
 	// chose to provision the instance synchronously
+	return brokerapi.ProvisionedServiceSpec{}, nil
 }
 
 func (*vaultServiceBroker) LastOperation(instanceID string) (brokerapi.LastOperation, error) {
 	// If the broker provisions asynchronously, the Cloud Controller will poll this endpoint
 	// for the status of the provisioning operation.
 	// This also applies to deprovisioning (work in progress).
+	return brokerapi.LastOperation{}, nil
 }
 
 func (*vaultServiceBroker) Deprovision(instanceID string, details brokerapi.DeprovisionDetails, asyncAllowed bool) (brokerapi.IsAsync, error) {
 	// Deprovision a new instance here. If async is allowed, the broker can still
 	// chose to deprovision the instance synchronously, hence the first return value.
+	return brokerapi.IsAsync(false), nil
 }
 
 func (*vaultServiceBroker) Bind(instanceID, bindingID string, details brokerapi.BindDetails) (brokerapi.Binding, error) {
 	// Bind to instances here
 	// Return a binding which contains a credentials object that can be marshalled to JSON,
 	// and (optionally) a syslog drain URL.
+	return brokerapi.Binding{}, nil
 }
 
 func (*vaultServiceBroker) Unbind(instanceID, bindingID string, details brokerapi.UnbindDetails) error {
 	// Unbind from instances here
+	return nil
 }
 
 func (*vaultServiceBroker) Update(instanceID string, details brokerapi.UpdateDetails, asyncAllowed bool) (brokerapi.IsAsync, error) {
 	// Update instance here
+	return brokerapi.IsAsync(false), nil
 }
 
 func main() {
